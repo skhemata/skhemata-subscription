2694e72f788c8c09f3219867ace0f76da6152f0b
Ryan <ryan.pitts@skhemata.com>
2021-07-27 13:57:54 -0700

    remove uppercase letters from ID generator

--- src/directives.ts
+++ src/directives.ts
@@ -1,6 +1,5 @@
 export const genId = (length: number) => {
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
   return Array(length)
     .fill('')
     .map(() => characters.charAt(Math.floor(Math.random() * characters.length)))
